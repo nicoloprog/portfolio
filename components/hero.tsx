32e127b612890e71e70a2b038c76d51d3f2594bd
nicoloprog <nicosubywrx@gmail.com>
2026-02-04 14:07:41 -0500

    hero spacing

--- components/hero.tsx
+++ components/hero.tsx
@@ -24,7 +24,7 @@ export default function Hero() {
               Développeur full-stack
             </span>
           </h1>
-          <p className="text-lg text-slate-400 mb-4 max-w-2xl">
+          <p className="text-md md:text-lg text-slate-400 mb-4 max-w-2xl">
             Développement de projets complets, de la conception à la mise en
             production, avec une attention portée à la performance, à la
             maintenabilité et à l'expérience utilisateur.
@@ -32,14 +32,14 @@ export default function Hero() {
           <div className="flex flex-wrap gap-4">
             <Link
               href="#projects" // ← replace with your path
-              className="h-12 px-8 inline-flex items-center justify-center rounded-full bg-primary text-gray-300 text-background-dark font-bold hover:bg-gray-700/30 transition-all glow-effect"
+              className="text-sm h-10 px-6 inline-flex items-center justify-center rounded-full bg-primary text-gray-300 text-background-dark font-bold hover:bg-gray-700/30 transition-all glow-effect"
             >
               Freelance
             </Link>
 
             <Link
               href="#contact" // ← replace with your path
-              className="h-12 px-8 inline-flex items-center justify-center rounded-full border text-gray-300 border-white/20 hover:bg-gray-700/30 transition-all"
+              className="text-smh-10 px-6 inline-flex items-center justify-center rounded-full border text-gray-300 border-white/20 hover:bg-gray-700/30 transition-all"
             >
               Contactez-moi
             </Link>
